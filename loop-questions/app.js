60d626334694dde1aadaaf14bdfede829d45634e
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-03-20 01:51:22 -0700

    Commit 9 - Created loops.js file

---
+++ loop-questions/app.js
@@ -0,0 +1,44 @@
+//Create a loop that logs the numbers from 0-99, (ascending)
+for (let i = 0; i <= 99; i++) {
+    console.log(i);   
+}
+
+// Create a loop that logs the numbers from 99-0 (descending)
+for (let i = 99; i >= 0; i--) {
+    console.log(i);
+}
+
+//Create a loop that logs the numbers from 298-1, counting down by 3 
+//(the code in the loop should run 99 times)
+for (let i = 298; i >= 1; i-=3) {
+    console.log(i)
+}
+
+//Create a loop that logs the EVEN numbers from 0-98
+for (let i = 0; i <= 98; i+=2) {
+    console.log(i);
+}
+
+//Create a loop that logs the ODD numbers from 98-0 (descending)
+for (let i = 98; i > 0; i--) {
+    if (i % 2 !== 0) {
+        console.log(i);
+        
+    }
+}
+
+//Create a loop that logs the numbers from 49-72 (ascending)
+for (let i = 49; i <= 79; i++){
+    console.log(i);
+}
+
+//Create a loop that logs the numbers from 81-26 (descending)
+for (let i = 81; i >= 26; i--) {
+    console.log(i);
+}
+
+//Create a loop that logs the numbers from 1-91 
+//that are multiples of 3 (ascending)
+for (let i = 3; i <= 91; i+=3) {
+        console.log(i);
+    }
